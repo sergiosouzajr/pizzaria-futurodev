eb6f6f45ecd858fac774271acd94a7849b73fe2c
sergiosouzajr <114541478+sergiosouzajr@users.noreply.github.com>
2022-10-14 21:10:44 -0300

    iniciar commit

---
+++ src/main/java/br/com/agls/pizzariafuturodev/PizzariaFuturodevApplication.java
@@ -0,0 +1,13 @@
+package br.com.agls.pizzariafuturodev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaFuturodevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaFuturodevApplication.class, args);
+	}
+
+}
